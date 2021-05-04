78995c72a1c25d7ec02d5fca8e7a9333e063c9b8
Ken Domino <kenneth.domino@domemtech.com>
2021-05-04 11:00:24 -0400

    Release 0.6.2. Doesn't work on asm/masm, Java target.

--- trgen/CGen.cs
+++ trgen/CGen.cs
@@ -749,7 +749,7 @@ Examples:
                         );
 
                 if (config.grammar_name + ".g4" == f &&
-                    !(f == this.parser_src_grammar_file_name || f == this.lexer_src_grammar_file_name))
+                    !(f == this.parser_grammar_file_name || f == this.lexer_grammar_file_name))
                 {
                     continue;
                 }
