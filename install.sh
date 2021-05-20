fd67bfd21adb3892d835270723035033f833ea84
Ken Domino <kenneth.domino@domemtech.com>
2021-05-20 14:26:45 -0400

    Update for optional specific version.

--- install.sh
+++ install.sh
@@ -1,25 +1,26 @@
-version=0.7.0
+version=""
+#version="--version 0.7.0"
 
-dotnet tool install -g tranalyze --version $version
-dotnet tool install -g trconvert --version $version
-dotnet tool install -g trfold --version $version
-dotnet tool install -g trfoldlit --version $version
-dotnet tool install -g trgen --version $version
-dotnet tool install -g trgroup --version $version
-dotnet tool install -g trjson --version $version
-dotnet tool install -g trkleene --version $version
-dotnet tool install -g trparse --version $version
-dotnet tool install -g trprint --version $version
-dotnet tool install -g trrename --version $version
-dotnet tool install -g trst --version $version
-dotnet tool install -g trstrip --version $version
-dotnet tool install -g trtext --version $version
-dotnet tool install -g trtokens --version $version
-dotnet tool install -g trtree --version $version
-dotnet tool install -g trunfold --version $version
-dotnet tool install -g trungroup --version $version
-dotnet tool install -g trwdog --version $version
-dotnet tool install -g trxgrep --version $version
-dotnet tool install -g trxml --version $version
-dotnet tool install -g trxml2 --version $version
+dotnet tool install -g tranalyze $version
+dotnet tool install -g trconvert $version
+dotnet tool install -g trfold $version
+dotnet tool install -g trfoldlit $version
+dotnet tool install -g trgen $version
+dotnet tool install -g trgroup $version
+dotnet tool install -g trjson $version
+dotnet tool install -g trkleene $version
+dotnet tool install -g trparse $version
+dotnet tool install -g trprint $version
+dotnet tool install -g trrename $version
+dotnet tool install -g trst $version
+dotnet tool install -g trstrip $version
+dotnet tool install -g trtext $version
+dotnet tool install -g trtokens $version
+dotnet tool install -g trtree $version
+dotnet tool install -g trunfold $version
+dotnet tool install -g trungroup $version
+dotnet tool install -g trwdog $version
+dotnet tool install -g trxgrep $version
+dotnet tool install -g trxml $version
+dotnet tool install -g trxml2 $version
 
