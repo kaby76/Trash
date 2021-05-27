2283aca271ad9c7f39b681172785265d6eae5bd8
Ken Domino <kenneth.domino@domemtech.com>
2021-05-27 12:15:51 -0400

    Fixes for next release.

--- publish.sh
+++ publish.sh
@@ -1,24 +1,18 @@
-version=0.7.0
-
-pushd "tranalyze/bin/Debug/"; dotnet nuget push tranalyze.$version.nupkg --api-key $trashkey --source https://api.nuget.org/v3/index.json; popd
-pushd "trconvert/bin/Debug/"; dotnet nuget push trconvert.$version.nupkg --api-key $trashkey --source https://api.nuget.org/v3/index.json; popd
-pushd "trfold/bin/Debug/"; dotnet nuget push trfold.$version.nupkg --api-key $trashkey --source https://api.nuget.org/v3/index.json; popd
-pushd "trfoldlit/bin/Debug/"; dotnet nuget push trfoldlit.$version.nupkg --api-key $trashkey --source https://api.nuget.org/v3/index.json; popd
-pushd "trgen/bin/Debug/"; dotnet nuget push trgen.$version.nupkg --api-key $trashkey --source https://api.nuget.org/v3/index.json; popd
-pushd "trgroup/bin/Debug/"; dotnet nuget push trgroup.$version.nupkg --api-key $trashkey --source https://api.nuget.org/v3/index.json; popd
-pushd "trjson/bin/Debug/"; dotnet nuget push trjson.$version.nupkg --api-key $trashkey --source https://api.nuget.org/v3/index.json; popd
-pushd "trkleene/bin/Debug/"; dotnet nuget push trkleene.$version.nupkg --api-key $trashkey --source https://api.nuget.org/v3/index.json; popd
-pushd "trparse/bin/Debug/"; dotnet nuget push trparse.$version.nupkg --api-key $trashkey --source https://api.nuget.org/v3/index.json; popd
-pushd "trprint/bin/Debug/"; dotnet nuget push trprint.$version.nupkg --api-key $trashkey --source https://api.nuget.org/v3/index.json; popd
-pushd "trrename/bin/Debug/"; dotnet nuget push trrename.$version.nupkg --api-key $trashkey --source https://api.nuget.org/v3/index.json; popd
-pushd "trst/bin/Debug/"; dotnet nuget push trst.$version.nupkg --api-key $trashkey --source https://api.nuget.org/v3/index.json; popd
-pushd "trstrip/bin/Debug/"; dotnet nuget push trstrip.$version.nupkg --api-key $trashkey --source https://api.nuget.org/v3/index.json; popd
-pushd "trtext/bin/Debug/"; dotnet nuget push trtext.$version.nupkg --api-key $trashkey --source https://api.nuget.org/v3/index.json; popd
-pushd "trtokens/bin/Debug/"; dotnet nuget push trtokens.$version.nupkg --api-key $trashkey --source https://api.nuget.org/v3/index.json; popd
-pushd "trtree/bin/Debug/"; dotnet nuget push trtree.$version.nupkg --api-key $trashkey --source https://api.nuget.org/v3/index.json; popd
-pushd "trunfold/bin/Debug/"; dotnet nuget push trunfold.$version.nupkg --api-key $trashkey --source https://api.nuget.org/v3/index.json; popd
-pushd "trungroup/bin/Debug/"; dotnet nuget push trungroup.$version.nupkg --api-key $trashkey --source https://api.nuget.org/v3/index.json; popd
-pushd "trwdog/bin/Debug/"; dotnet nuget push trwdog.$version.nupkg --api-key $trashkey --source https://api.nuget.org/v3/index.json; popd
-pushd "trxgrep/bin/Debug/"; dotnet nuget push trxgrep.$version.nupkg --api-key $trashkey --source https://api.nuget.org/v3/index.json; popd
-pushd "trxml/bin/Debug/"; dotnet nuget push trxml.$version.nupkg --api-key $trashkey --source https://api.nuget.org/v3/index.json; popd
-pushd "trxml2/bin/Debug/"; dotnet nuget push trxml2.$version.nupkg --api-key $trashkey --source https://api.nuget.org/v3/index.json; popd
+version=0.8.0
+for i in tranalyze trconvert trfold trfoldlit trgen trgroup trjson trkleene trparse trprint trrename trst trstrip trtext trtokens trtree trunfold trungroup trwdog trxgrep trxml trxml2
+do
+	echo $i
+	if [[ ! -d $i ]]
+	then
+		continue
+	fi
+	pushd $i
+	count=`ls *.csproj | wc | awk '{print $1}'`
+	if [[ $count != 1 ]]
+	then
+		continue
+	fi
+	cd bin/Debug
+	dotnet nuget push $i.$version.nupkg --api-key $trashkey --source https://api.nuget.org/v3/index.json
+	popd
+done
--- version.sh
+++ version.sh
@@ -1,6 +1,6 @@
 version="0.8.0"
 
-for i in tranalyze trconvert trfold trfoldlit trgen trgroup trjson trkleene trparse trprint trrename trst trstrip trtext trunfold trungroup trwdog trxgrep trxml trxml2
+for i in tranalyze trconvert trfold trfoldlit trgen trgroup trjson trkleene trparse trprint trrename trst trstrip trtext trtokens trtree trunfold trungroup trwdog trxgrep trxml trxml2
 do
 	echo $i
 	if [[ ! -d $i ]]
