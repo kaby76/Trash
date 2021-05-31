c11e156e301d8ab7875b9db36d08003a64d23be9
Ken Domino <kenneth.domino@domemtech.com>
2021-05-31 07:28:07 -0400

    Simplify scripts and bump tragl version.

--- _scripts/publish.sh
+++ _scripts/publish.sh
@@ -2,16 +2,7 @@ version=0.8.1
 for i in tranalyze trcombine trconvert trdelete trfold trfoldlit trgen trgroup trjson trkleene trparse trprint trrename trsplit trsponge trst trstrip trtext trtokens trtree trunfold trungroup trwdog trxgrep trxml trxml2
 do
 	echo $i
-	if [[ ! -d $i ]]
-	then
-		continue
-	fi
 	pushd $i
-	count=`ls *.csproj | wc | awk '{print $1}'`
-	if [[ $count != 1 ]]
-	then
-		continue
-	fi
 	cd bin/Debug
 	dotnet nuget push $i.$version.nupkg --api-key $trashkey --source https://api.nuget.org/v3/index.json
 	popd
--- _scripts/version.sh
+++ _scripts/version.sh
@@ -3,16 +3,16 @@ version="0.8.1"
 for i in tranalyze trcombine trconvert trdelete trfold trfoldlit trgen trgroup trjson trkleene trparse trprint trrename trsplit trsponge trst trstrip trtext trtokens trtree trunfold trungroup trwdog trxgrep trxml trxml2
 do
 	echo $i
-	if [[ ! -d $i ]]
-	then
-		continue
-	fi
 	pushd $i
-	count=`ls *.csproj | wc | awk '{print $1}'`
-	if [[ $count != 1 ]]
-	then
-		continue
-	fi
+	rm -f asdfasdf
+	cat *.csproj | sed -e "s%[<][Vv]ersion[>].*[<][/][Vv]ersion[>]%<Version\>$version</Version>%" > asdfasdf
+	mv asdfasdf *.csproj	
+	popd
+done
+for i in tragl
+do
+	echo $i
+	pushd $i
 	rm -f asdfasdf
 	cat *.csproj | sed -e "s%[<][Vv]ersion[>].*[<][/][Vv]ersion[>]%<Version\>$version</Version>%" > asdfasdf
 	mv asdfasdf *.csproj	
