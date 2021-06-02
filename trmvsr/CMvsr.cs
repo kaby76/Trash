00a0f289830a319166fe142144852af2fd14c8bf
Ken Domino <kenneth.domino@domemtech.com>
2021-06-02 08:06:39 -0400

    Add trmvsr.

--- trmvsr/CMvsr.cs
+++ trmvsr/CMvsr.cs
@@ -1,35 +1,86 @@
-﻿namespace Trash.Commands
+﻿namespace Trash
 {
+    using Antlr4.Runtime.Tree;
+    using AntlrJson;
     using LanguageServer;
     using org.eclipse.wst.xml.xpath2.processor.util;
+    using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
+    using System.Text.Json;
 
     class CMvsr
     {
-        public void Help()
+        public string Help()
         {
-            System.Console.WriteLine(@"mvsr <string>
-Move the rule, whose symbol is identified by the xpath string, to the top of the grammar.
-");
+            using (Stream stream = this.GetType().Assembly.GetManifestResourceStream("trmvsr.readme.md"))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
         }
 
-        public void Execute(Repl repl, ReplParser.MvsrContext tree, bool piped)
+        public void Execute(Config config)
         {
-            var expr = tree.StringLiteral().GetText();
-            expr = expr.Substring(1, expr.Length - 2);
-            var doc = repl.stack.Peek();
-            var pr = ParsingResultsFactory.Create(doc);
-            var aparser = pr.Parser;
-            var atree = pr.ParseTree;
-            using (AntlrTreeEditing.AntlrDOM.AntlrDynamicContext dynamicContext = new AntlrTreeEditing.AntlrDOM.ConvertToDOM().Try(atree, aparser))
+            var expr = config.Expr.First();
+            System.Console.Error.WriteLine("Expr = '" + expr + "'");
+            string lines = null;
+            if (!(config.File != null && config.File != ""))
             {
-                org.eclipse.wst.xml.xpath2.processor.Engine engine = new org.eclipse.wst.xml.xpath2.processor.Engine();
-                var nodes = engine.parseExpression(expr,
-                        new StaticContextBuilder()).evaluate(dynamicContext, new object[] { dynamicContext.Document })
-                    .Select(x => (x.NativeValue as AntlrTreeEditing.AntlrDOM.AntlrElement).AntlrIParseTree).ToList();
-                var results = LanguageServer.Transform.MoveStartRuleToTop(doc, nodes);
-                repl.EnactEdits(results);
+                for (; ; )
+                {
+                    lines = System.Console.In.ReadToEnd();
+                    if (lines != null && lines != "") break;
+                }
             }
+            else
+            {
+                lines = File.ReadAllText(config.File);
+            }
+            var serializeOptions = new JsonSerializerOptions();
+            serializeOptions.Converters.Add(new ParseTreeConverter());
+            serializeOptions.WriteIndented = true;
+            var data = JsonSerializer.Deserialize<ParsingResultSet[]>(lines, serializeOptions);
+            var results = new List<ParsingResultSet>();
+            foreach (var parse_info in data)
+            {
+                var text = parse_info.Text;
+                var fn = parse_info.FileName;
+                var atrees = parse_info.Nodes;
+                var parser = parse_info.Parser;
+                var lexer = parse_info.Lexer;
+                var tokstream = parse_info.Stream;
+                var doc = Docs.Class1.CreateDoc(parse_info);
+                doc.ParseTree = null;
+                doc.Changed = true;
+                ParsingResults ref_pd = ParsingResultsFactory.Create(doc);
+                ref_pd.ParseTree = null;
+                org.eclipse.wst.xml.xpath2.processor.Engine engine = new org.eclipse.wst.xml.xpath2.processor.Engine();
+                IParseTree root = atrees.First().Root();
+                var ate = new AntlrTreeEditing.AntlrDOM.ConvertToDOM();
+                using (AntlrTreeEditing.AntlrDOM.AntlrDynamicContext dynamicContext = ate.Try(root, parser))
+                {
+                    var nodes = engine.parseExpression(expr,
+                            new StaticContextBuilder()).evaluate(dynamicContext, new object[] { dynamicContext.Document })
+                        .Select(x => (x.NativeValue as AntlrTreeEditing.AntlrDOM.AntlrElement).AntlrIParseTree).ToList();
+                    var res = LanguageServer.Transform.MoveStartRuleToTop(doc, nodes);
+                    var pr = ParsingResultsFactory.Create(doc);
+                    IParseTree pt = pr.ParseTree;
+                    var tuple = new ParsingResultSet()
+                    {
+                        Text = doc.Code,
+                        FileName = doc.FullPath,
+                        Stream = pr.TokStream,
+                        Nodes = new IParseTree[] { pt },
+                        Lexer = pr.Lexer,
+                        Parser = pr.Parser
+                    };
+                    results.Add(tuple);
+                }
+            }
+            string js1 = JsonSerializer.Serialize(results.ToArray(), serializeOptions);
+            System.Console.Write(js1);
+
         }
     }
 }
---
+++ trmvsr/readme.md
@@ -0,0 +1,14 @@
+# Trmvsr
+
+Move the rule, whose symbol is identified by the xpath string, to the top of the grammar.
+
+# Usage
+
+    trmvsr <string>
+
+# Example
+
+
+# Current version
+
+0.8.1 -- Improved help and documentation.
