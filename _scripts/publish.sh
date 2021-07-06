49ba77beea1f606b2f4c0ce12152d7af9ae97811
Ken Domino <kenneth.domino@domemtech.com>
2021-07-05 21:48:42 -0400

    Update publish release.

--- _scripts/publish.sh
+++ _scripts/publish.sh
@@ -1,4 +1,4 @@
-version=0.8.4
+version=0.8.5
 for i in tranalyze trcombine trconvert trdelabel trdelete trdot trfold trfoldlit trgen trgroup trjson trkleene trmvsr trparse trprint trrename trsplit trsponge trst trstrip trtext trthompson trtokens trtree trunfold trungroup trwdog trxgrep trxml trxml2
 do
 	echo $i
