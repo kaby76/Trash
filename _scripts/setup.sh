310cba4382e1d1e5871d09a026872c946d0c218c
Ken Domino <kenneth.domino@domemtech.com>
2021-07-19 07:44:51 -0400

    Fix script for Linux.

--- _scripts/setup.sh
+++ _scripts/setup.sh
@@ -8,12 +8,14 @@ case "${unameOut}" in
     MINGW*)     machine=MinGw;;
     *)          machine="UNKNOWN:${unameOut}"
 esac
-if [[ "$machine" != "MinGW" ]]
+if [[ "$machine" == "MinGw" ]]
 then
     cwd=`pwd | sed 's%/c%c:%' | sed 's%/%\\\\%g'`
 else
     cwd=`pwd`
 fi
+echo "$machine"
+echo "$cwd"
 for i in tranalyze trcombine trconvert trdelabel trdelete trdot trfold trfoldlit trgen trgroup trinsert trjson trkleene trmvsr trparse trprint trrename trsplit trsponge trst trstrip trtext trthompson trtokens trtree trunfold trungroup trwdog trxgrep trxml trxml2
 do
     echo $i
