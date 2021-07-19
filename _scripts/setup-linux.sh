32cb5ffebfc5d013c510f6f618810da9ff9b8a3b
kaby76 <ken.domino@gmail.com>
2021-07-19 06:44:35 -0400

    Updates.

---
+++ _scripts/setup-linux.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+    cwd=`pwd`
+echo $cwd
+for i in tranalyze trcombine trconvert trdelabel trdelete trdot trfold trfoldlit trgen trgroup trinsert trjson trkleene trmvsr trparse trprint trrename trsplit trsponge trst trstrip trtext trthompson trtokens trtree trunfold trungroup trwdog trxgrep trxml trxml2
+do
+    echo $i
+    dotnet nuget add source $cwd/$i/bin/Debug/ --name nuget-$i
+done
+dotnet nuget list source
