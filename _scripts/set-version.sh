7804e3f52455f172417b59bb4e8ddfa90d294e79
Ken Domino <kenneth.domino@domemtech.com>
2021-09-18 08:16:58 -0400

    Update versions.

--- _scripts/set-version.sh
+++ _scripts/set-version.sh
@@ -1,4 +1,4 @@
-version="0.10.0"
+version="0.11.0"
 
 for i in tranalyze trcombine trconvert trdelabel trdelete trdot trfold trfoldlit trformat trgen trgroup trinsert trjson trkleene trmvsr trparse trprint trrename trrr trrup trsplit trsponge trst trstrip trtext trthompson trtokens trtree trunfold trungroup trwdog trxgrep trxml trxml2
 do
