48e4a50d01c4c838fb0f5381f2545e9f86964123
Ken Domino <kenneth.domino@domemtech.com>
2021-10-14 17:03:11 -0400

    Update trrup test--with errors.

---
+++ _tests/trrup/Gold/t3.g4
@@ -0,0 +1,5 @@
+grammar t3;
+a :a;
+b :a b c;
+c :a b* | c;
+d : (a b | c);
--- _tests/trrup/test.sh
+++ _tests/trrup/test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 trparse t1.g4 | trrup | trsponge -o Gold -c
 trparse t2.g4 | trrup | trsponge -o Gold -c
+trparse t3.g4 | trrup | trsponge -o Gold -c
