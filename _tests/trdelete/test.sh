6cc4c56de93ceef94e97213f87bbc48d26e4b2e9
Ken Domino <kenneth.domino@domemtech.com>
2022-08-20 08:54:19 -0400

    Add test of trdelete.

--- _tests/trdelete/test.sh
+++ _tests/trdelete/test.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-echo "Setting MSYS2_ARG_CONV_EXCL so that Trash XPaths do not get mutulated."
+# "Setting MSYS2_ARG_CONV_EXCL so that Trash XPaths do not get mutulated."
 export MSYS2_ARG_CONV_EXCL="*"
-
-trparse Expression.g4 | trdelete '//parserRuleSpec[RULE_REF/text()="a"]' | trsponge -c -o Gold
+where=`dirname -- "$0"`
+trparse "$where/Expression.g4" | trdelete '//parserRuleSpec[RULE_REF/text()="a"]' | trsponge -c -o "$where/Generated"
+diff -r "$where/Gold" "$where/Generated"
+if [ "$?" != "0" ]
+then
+	echo Test failed.
+	exit 1
+else
+	echo Test succeeded.
+fi
