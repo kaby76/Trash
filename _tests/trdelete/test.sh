177f8aff9f35ebe3f5ec297eb7bb5f2fe90cd6aa
Ken Domino <kenneth.domino@domemtech.com>
2022-08-20 12:42:48 -0400

    Fix typo.

--- _tests/trdelete/test.sh
+++ _tests/trdelete/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # "Setting MSYS2_ARG_CONV_EXCL so that Trash XPaths do not get mutulated."
-oexport MSYS2_ARG_CONV_EXCL="*"
+export MSYS2_ARG_CONV_EXCL="*"
 where=`dirname -- "$0"`
 trparse "$where/Expression.g4" | trdelete '//parserRuleSpec[RULE_REF/text()="a"]' | trsponge -c -o "$where/Generated"
 diff -r "$where/Gold" "$where/Generated"
