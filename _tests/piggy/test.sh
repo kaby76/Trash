fc5071df270b93062dc58d360a71914b07ec1c48
Ken Domino <kenneth.domino@domemtech.com>
2022-08-28 21:07:51 -0400

    Almost there. Why isn't trparse working?

--- _tests/piggy/test.sh
+++ _tests/piggy/test.sh
@@ -29,6 +29,11 @@ echo PWD:
 pwd
 echo ls -R
 ls -R
-trparse -p "antlr4/Generated" "Repeat.g4" | trpiggy "repeat.pig" | trtext > "Generated/Repeat.g4"
+echo trparse -p "antlr4/Generated" "Repeat.g4"
+trparse -p "antlr4/Generated" "Repeat.g4" > o1.pt
+echo trpiggy "repeat.pig"
+cat o1.pt | trpiggy "repeat.pig" > o2.pt
+echo trtext
+cat o2.pt | trtext > "Generated/Repeat.g4"
 diff -r "Gold" "Generated"
 echo "Test suceeded."
