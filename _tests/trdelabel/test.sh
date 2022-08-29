18cfc463c611302b0a3331986c694ce529f993cb
Ken Domino <kenneth.domino@domemtech.com>
2022-08-29 10:18:30 -0400

    Another problem with trparse.

--- _tests/trdelabel/test.sh
+++ _tests/trdelabel/test.sh
@@ -2,15 +2,16 @@
 
 export MSYS2_ARG_CONV_EXCL="*"
 where=`dirname -- "$0"`
-for i in "$where/*.g4"
+cd "$where"
+for i in "*.g4"
 do
 	echo $i
 	extension="${i##*.}"
 	filename="${i%.*}"
-	trparse $i | trdelabel | trsponge -c -o "$where/Generated"
+	trparse $i | trdelabel | trsponge -c -o "Generated"
 done
-rm -f "$where"/Generated/*.txt2
-diff -r "$where/Gold" "$where/Generated"
+rm -f Generated/*.txt2
+diff -r "Gold" "Generated"
 if [ "$?" != "0" ]
 then
 	echo Test failed.
