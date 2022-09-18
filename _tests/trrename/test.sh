49f503caadd34a62f8ea31eebd4466c76f29ba6f
Ken Domino <kenneth.domino@domemtech.com>
2022-09-18 13:22:48 -0400

    Fix trrename test.

--- _tests/trrename/test.sh
+++ _tests/trrename/test.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
-export MSYS2_ARG_CONV_EXCL="*"
+where=`dirname -- "$0"`
+cd $where
 rm -rf Generated
+export MSYS2_ARG_CONV_EXCL="*"
 trparse Expression.g4 | trrename -r 'e,exp;a,atom;INT,Int;MUL,OpMul;DIV,OpDiv;ADD,OpAdd;SUB,OpSub' | trsponge -c -o Generated
 diff -r Gold Generated
 if [ "$?" != "0" ]
