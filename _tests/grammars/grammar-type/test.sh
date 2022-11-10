b6b074ec893cc4037b3ab540990a8d2716df04e9
Ken Domino <kenneth.domino@domemtech.com>
2022-11-10 06:12:05 -0500

    We cannot rely on order of Bash glob expansion. Force the sort.

--- _tests/grammars/grammar-type/test.sh
+++ _tests/grammars/grammar-type/test.sh
@@ -6,7 +6,7 @@ where=`dirname -- "$0"`
 cd "$where"
 rm -rf Generated
 mkdir Generated
-for i in examples/*
+for i in `echo examples/* | sort -u`
 do
 	bash get-types.sh $i >> Generated/output
 done
