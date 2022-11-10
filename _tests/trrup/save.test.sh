90c77764df1b5c3d095b68589e757c007163c8e9
Ken Domino <kenneth.domino@domemtech.com>
2022-11-10 17:38:42 -0500

    Fix trrup test.

--- _tests/trrup/save.test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-trparse t1.g4 | trrup | trsponge -o Gold -c
-trparse t2.g4 | trrup | trsponge -o Gold -c
-trparse t3.g4 | trrup | trsponge -o Gold -c
-trparse t4.g4 | trrup | trsponge -o Gold -c
---
+++ _tests/trrup/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# "Setting MSYS2_ARG_CONV_EXCL so that Trash XPaths do not get mutulated."
+export MSYS2_ARG_CONV_EXCL="*"
+where=`dirname -- "$0"`
+cd "$where"
+rm -rf Generated
+trparse t1.g4 | trrup | trsponge -c -o Generated
+trparse t2.g4 | trrup | trsponge -c -o Generated
+trparse t3.g4 | trrup | trsponge -c -o Generated
+trparse t4.g4 | trrup | trsponge -c -o Generated
+for i in "Generated/*"
+do
+	dos2unix $i
+done
+for i in "Gold/*"
+do
+	dos2unix $i
+done
+diff -r "Gold" "Generated"
+if [ "$?" != "0" ]
+then
+	echo Test failed.
+	exit 1
+else
+	echo Test succeeded.
+fi
