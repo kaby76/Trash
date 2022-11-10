8da5b8ee1a0eaf6c85e0bf7fded90dcbf370d8b6
Ken Domino <kenneth.domino@domemtech.com>
2022-11-10 06:28:02 -0500

    Try another way by pushing it through "ls | sort".

--- _tests/grammars/grammar-type/test.sh
+++ _tests/grammars/grammar-type/test.sh
@@ -6,7 +6,7 @@ where=`dirname -- "$0"`
 cd "$where"
 rm -rf Generated
 mkdir Generated
-for i in `echo examples/* | sort -u`
+for i in `ls examples/* | sort -u`
 do
 	bash get-types.sh $i >> Generated/output
 done
