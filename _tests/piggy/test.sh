59bec19a3484980aa85f821e476ee80a2e4f3232
Ken Domino <kenneth.domino@domemtech.com>
2022-11-11 05:53:25 -0500

    Update piggy test.

--- _tests/piggy/test.sh
+++ _tests/piggy/test.sh
@@ -7,15 +7,17 @@ set -e
 trap 'LAST_COMMAND=$CURRENT_COMMAND; CURRENT_COMMAND=$BASH_COMMAND' DEBUG
 #   on error: print the failed command
 trap 'ERROR_CODE=$?; FAILED_COMMAND=$LAST_COMMAND; tput setaf 1; echo "ERROR: command \"$FAILED_COMMAND\" failed with exit code $ERROR_CODE"; put sgr0;' ERR INT TERM
-
 export MSYS2_ARG_CONV_EXCL="*"
 where=`dirname -- "$0"`
-cd "$where/antlr4"
-rm -rf "$where/Generated"
+cd "$where"
+where=`pwd`
+cd antlr4
 pwd
+rm -rf Generated
 trgen
-dotnet build Generated/Test.csproj
-cd ..
+cd Generated
+dotnet build
+cd "$where"
 rm -rf Generated
 mkdir Generated
 echo trparse -p "antlr4/Generated" "Repeat.g4"
@@ -24,5 +26,19 @@ echo trpiggy "repeat.pig"
 cat o1.pt | trpiggy "repeat.pig" > o2.pt
 echo trtext
 cat o2.pt | trtext > "Generated/Repeat.g4"
-diff -r "Gold" "Generated"
-echo "Test suceeded."
+for i in "$where/Generated/*"
+do
+	dos2unix $i
+done
+for i in "$where/Gold/*"
+do
+	dos2unix $i
+done
+diff -r "$where/Gold" "$where/Generated"
+if [ "$?" != "0" ]
+then
+	echo Test failed.
+	exit 1
+else
+	echo Test succeeded.
+fi
