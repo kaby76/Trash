68604369bd6fc4d9e4ff8d907a422ba5eba2d157
Ken Domino <kenneth.domino@domemtech.com>
2022-11-12 10:23:58 -0500

    Add test for trsplit and trcombine.

--- _tests/trcombine/test.sh
+++ _tests/trcombine/test.sh
@@ -12,10 +12,32 @@ trap 'ERROR_CODE=$?; FAILED_COMMAND=$LAST_COMMAND; tput setaf 1; echo "ERROR: co
 export MSYS2_ARG_CONV_EXCL="*"
 where=`dirname -- "$0"`
 cd "$where"
+where=`pwd`
+cd "$where"
+
+# Test.
+rm -rf Generated
 trparse Arithmetic.g4 > o1.pt
 cat o1.pt | trsplit > o2.pt
 cat o2.pt | trsponge -c -o Generated
 trcombine Generated/ArithmeticLexer.g4 Generated/ArithmeticParser.g4 > o3.pt
 cat o3.pt | trprint > Generated/Arithmetic.g4
+
+# Diff.
+for i in "$where/Generated/*"
+do
+	dos2unix $i
+done
+for i in "$where/Gold/*"
+do
+	dos2unix $i
+done
 diff -r Gold/Arithmetic.g4 Generated/Arithmetic.g4
-echo Test succeeded.
+if [ "$?" != "0" ]
+then
+	echo Test failed.
+	exit 1
+else
+	echo Test succeeded.
+fi
+exit 0
