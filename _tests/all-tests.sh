0f1d2e6f0fff37cf2ff0cec2b9f89c9c8a667240
Ken Domino <kenneth.domino@domemtech.com>
2022-11-12 08:57:52 -0500

    Updates to test all and report all failed.

--- _tests/all-tests.sh
+++ _tests/all-tests.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-set +e
-set -x
+#set +e
+#set -x
 export TERM=xterm-mono
 export MSYS2_ARG_CONV_EXCL="*"
 where=`dirname -- "$0"`
-tests=`find "$where" -name test.sh | grep -v Generated`
-echo $tests
+tests=`find "$where" -name test.sh | grep -v Generated | sort`
+echo Tests in Trash: $tests
+failed=()
 for i in $tests
 do
 	bash $i
 	result=$?
 	if [ "$result" != 0 ]
 	then
-		exit 1
+		failed+=($i)
 	fi
 done
+if (( ${#failed[@]} != 0 ))
+then
+	echo Tests failed: $failed.
+	exit 1
+else
+	echo Tests succeeded.
+	exit 0
+fi
+
