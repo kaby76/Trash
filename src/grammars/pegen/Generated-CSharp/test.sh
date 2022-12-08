f44f693705dde23ddd2efeb337f63154038447b8
Ken Domino <kenneth.domino@domemtech.com>
2022-12-08 14:12:32 -0500

    Everything in generated internal grammar builds should be considered source that needs to be checked in.

---
+++ src/grammars/pegen/Generated-CSharp/test.sh
@@ -0,0 +1,33 @@
+# Template generated code from trgen 0.18.2
+err=0
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+for g in `find ../examples -type f | grep -v '.errors$' | grep -v '.tree$'`
+do
+  file="$g"
+  x1="${g##*.}"
+  if [ "$x1" != "errors" ]
+  then
+    echo "$file"
+    cat "$file" | trwdog ./bin/Debug/net6.0/Test.exe
+    status="$?"
+    if [ -f "$file".errors ]
+    then
+      if [ "$stat" = "0" ]
+      then
+        echo Expected parse fail.
+        err=1
+        break
+      else
+        echo Expected.
+      fi
+    else
+      if [ "$status" != "0" ]
+      then
+        err=1
+        break
+      fi
+    fi
+  fi
+done
+exit $err
