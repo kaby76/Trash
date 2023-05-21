d6c6ca94bf1ec7888e5a5b485a79d11954938721
Ken Domino <kenneth.domino@domemtech.com>
2023-05-21 06:08:12 -0400

    Switch order of rules of determining how to parse args. This change saves enormous time in useless file system searching.

--- src/trparse/Command.cs
+++ src/trparse/Command.cs
@@ -28,61 +28,7 @@ namespace Trash
             // If Type=="gen", then parse using Generated/.
             // If Type=="antlr2", then parse using Antlr2.
             // Etc.
-
-            string path = config.ParserLocation != null ? config.ParserLocation
-                : Environment.CurrentDirectory + Path.DirectorySeparatorChar;
-            if (!Directory.Exists(path))
-                throw new Exception("Path of parser does not exist.");
-            path = Path.GetFullPath(path);
-            path = path.Replace("\\", "/");
-            if (!path.EndsWith("/")) path = path + "/";
-            var fp = new TrashGlobbing.Glob(path)
-                .RegexContents("(Generated-CSharp/)?bin/.*(?!ref)/" + config.Dll + ".dll$")
-                .Where(f => f is FileInfo && !f.Attributes.HasFlag(FileAttributes.Directory))
-                .Select(f => f.FullName.Replace('\\', '/'))
-                .ToList();
-            var exists = fp.Count == 1;
-            if (config.ParserLocation != null && !exists)
-            {
-                System.Console.Error.WriteLine("Parser doesn't exist");
-                System.Console.Error.WriteLine("fp " + String.Join(" ", fp));
-                var is_generated_cs = new TrashGlobbing.Glob(path)
-                    .RegexContents("(Generated-CSharp/)*.cs")
-                    .Where(f => f is FileInfo && !f.Attributes.HasFlag(FileAttributes.Directory))
-                    .Select(f => f.FullName.Replace('\\', '/'))
-                    .ToList();
-                System.Console.Error.WriteLine("is_generated_cs = " + String.Join(" ", is_generated_cs));
-                var is_generated_java = new TrashGlobbing.Glob(path)
-                    .RegexContents("(Generated-Java/)*.java")
-                    .Where(f => f is FileInfo && !f.Attributes.HasFlag(FileAttributes.Directory))
-                    .Select(f => f.FullName.Replace('\\', '/'))
-                    .ToList();
-                System.Console.Error.WriteLine("is_generated_java = " + String.Join(" ", is_generated_java));
-                if (is_generated_cs.Count > 0 && is_generated_java.Count == 0)
-                {
-                    throw new Exception("-p specified, but the parser doesn't exist. Did you do a 'dotnet build'?");
-                } else if (is_generated_cs.Count == 0 && is_generated_java.Count > 0)
-                {
-                    throw new Exception("-p specified, but the parser is a java program. Trparse doesn't work with that.");
-                } else if (is_generated_java.Count > 0 && is_generated_cs.Count > 0)
-                {
-                    throw new Exception("-p specified, but the parser is a mix of C# and Java. Trparse works with only a C# target parser."); ;
-                }
-                else if (is_generated_java.Count == 0 && is_generated_cs.Count == 0)
-                {
-                    throw new Exception("-p specified, but I don't see any C# or Java. Trparse works with only a C# target parser, and it must be built."); ;
-                }
-                throw new Exception("-p specified, but the parser doesn't exist.");
-            }
-            string full_path = null;
-            if (exists)
-            {
-                full_path = fp.First();
-                exists = File.Exists(full_path);
-            }
-
-            var grun = new Grun(config);
-            if (config.Type == null && !exists)
+            if (config.Type == null)
             {
                 // Come up with a type to parse.
                 // If there is no generated parser, then use one of the built-in parsers.
@@ -98,10 +44,76 @@ namespace Trash
                     ".gram" => "pegen_v3_10",
                     ".rex" => "rex",
                     ".y" => "Bison",
-                    _ => throw new Exception("Unknown file extension, cannot load in a built-in parser.")
+                    _ => null
                 };
                 config.Type = parser_type;
             }
+
+            if (config.Type == null)
+            {
+                string path = config.ParserLocation != null
+                    ? config.ParserLocation
+                    : Environment.CurrentDirectory + Path.DirectorySeparatorChar;
+                if (!Directory.Exists(path))
+                    throw new Exception("Path of parser does not exist.");
+                path = Path.GetFullPath(path);
+                path = path.Replace("\\", "/");
+                if (!path.EndsWith("/")) path = path + "/";
+                var fp = new TrashGlobbing.Glob(path)
+                    .RegexContents("(Generated-CSharp/)?bin/.*(?!ref)/" + config.Dll + ".dll$")
+                    .Where(f => f is FileInfo && !f.Attributes.HasFlag(FileAttributes.Directory))
+                    .Select(f => f.FullName.Replace('\\', '/'))
+                    .ToList();
+                var exists = fp.Count == 1;
+                if (config.ParserLocation != null && !exists)
+                {
+                    System.Console.Error.WriteLine("Parser doesn't exist");
+                    System.Console.Error.WriteLine("fp " + String.Join(" ", fp));
+                    var is_generated_cs = new TrashGlobbing.Glob(path)
+                        .RegexContents("(Generated-CSharp/)*.cs")
+                        .Where(f => f is FileInfo && !f.Attributes.HasFlag(FileAttributes.Directory))
+                        .Select(f => f.FullName.Replace('\\', '/'))
+                        .ToList();
+                    System.Console.Error.WriteLine("is_generated_cs = " + String.Join(" ", is_generated_cs));
+                    var is_generated_java = new TrashGlobbing.Glob(path)
+                        .RegexContents("(Generated-Java/)*.java")
+                        .Where(f => f is FileInfo && !f.Attributes.HasFlag(FileAttributes.Directory))
+                        .Select(f => f.FullName.Replace('\\', '/'))
+                        .ToList();
+                    System.Console.Error.WriteLine("is_generated_java = " + String.Join(" ", is_generated_java));
+                    if (is_generated_cs.Count > 0 && is_generated_java.Count == 0)
+                    {
+                        throw new Exception("-p specified, but the parser doesn't exist. Did you do a 'dotnet build'?");
+                    }
+                    else if (is_generated_cs.Count == 0 && is_generated_java.Count > 0)
+                    {
+                        throw new Exception(
+                            "-p specified, but the parser is a java program. Trparse doesn't work with that.");
+                    }
+                    else if (is_generated_java.Count > 0 && is_generated_cs.Count > 0)
+                    {
+                        throw new Exception(
+                            "-p specified, but the parser is a mix of C# and Java. Trparse works with only a C# target parser.");
+                        ;
+                    }
+                    else if (is_generated_java.Count == 0 && is_generated_cs.Count == 0)
+                    {
+                        throw new Exception(
+                            "-p specified, but I don't see any C# or Java. Trparse works with only a C# target parser, and it must be built.");
+                        ;
+                    }
+
+                    throw new Exception("-p specified, but the parser doesn't exist.");
+                }
+
+                string full_path = null;
+                if (exists)
+                {
+                    full_path = fp.First();
+                    exists = File.Exists(full_path);
+                }
+            }
+            var grun = new Grun(config);
             Environment.ExitCode = grun.Run(config.Type);
         }
     }
