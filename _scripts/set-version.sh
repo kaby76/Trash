f20c44ffc303268fbe0d6fbae37890104d8ae7f4
Ken Domino <kenneth.domino@domemtech.com>
2023-08-20 06:59:09 -0400

    Add missing to bump.

--- _scripts/set-version.sh
+++ _scripts/set-version.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-version="0.21.1"
+version="0.21.2"
 cd src
 directories=`find . -maxdepth 1 -type d -name "tr*"`
 cwd=`pwd`
@@ -41,7 +41,7 @@ do
 	mv asdfasdf *.csproj	
 	rm -f asdfasdf2
 	touch readme.md
-	cat readme.md | sed -e 's%^0[.][0-9]*[.][0-9]*.*$'"%$version"' Fixes to trgen for Cpp target. Update run.sh in templates.%' > asdfasdf2
+	cat readme.md | sed -e 's%^0[.][0-9]*[.][0-9]*.*$'"%$version"' Fix to trconvert for Bison. Add trquery. Fixes to trperf, trparse.%' > asdfasdf2
 	mv asdfasdf2 readme.md
 	cd ..
 done
@@ -78,7 +78,7 @@ do
 	mv asdfasdf *.csproj	
 	rm -f asdfasdf2
 	touch readme.md
-	cat readme.md | sed -e 's%^0[.][0-9]*[.][0-9]*.*$'"%$version"' Fixes to trgen for Cpp target. Update run.sh in templates.%' > asdfasdf2
+	cat readme.md | sed -e 's%^0[.][0-9]*[.][0-9]*.*$'"%$version"' Fix to trconvert for Bison. Add trquery. Fixes to trperf, trparse.%' > asdfasdf2
 	mv asdfasdf2 readme.md
 	cd ..
 done
--- src/trgen/templates/PHP/st.test.sh
+++ src/trgen/templates/PHP/st.test.sh
@@ -1,5 +1,8 @@
 # Generated from trgen <version>
 
+# comment for local dotnet tools.
+global=1
+
 # People often specify a test file directory, but sometimes no
 # tests are provided. Git won't check in an empty directory.
 # Test if the test file directory does not exist, or it is just
@@ -24,7 +27,12 @@ files2=`find ../<example_files_unix> -type f | grep -v '.errors$' | grep -v '.tr
 files=()
 for f in $files2
 do
-    dotnet triconv -- -f utf-8 $f > /dev/null 2>&1
+    if [ "$global" == "" ]
+    then
+        dotnet triconv -- -f utf-8 $f > /dev/null 2>&1
+    else
+        triconv -f utf-8 $f > /dev/null 2>&1
+    fi
     if [ "$?" = "0" ]
     then
         files+=( $f )
@@ -37,7 +45,12 @@ done
 rm -f parse.txt
 for f in ${files[*]}
 do
-    dotnet trwdog -- php -d memory_limit=1G Test.php -q -tee -tree $f >> parse.txt 2>&1
+    if [ "$global" == "" ]
+    then
+        dotnet trwdog -- php -d memory_limit=1G Test.php -q -tee -tree $f >> parse.txt 2>&1
+    else
+        trwdog php -d memory_limit=1G Test.php -q -tee -tree $f >> parse.txt 2>&1
+    fi
     xxx="$?"
     if [ "$xxx" -ne 0 ]
     then
@@ -46,7 +59,12 @@ do
 done
 <else>
 # Group parsing.
-echo "${files[*]}" | dotnet trwdog -- php -d memory_limit=1G Test.php -q -x -tee -tree > parse.txt 2>&1
+if [ "$global" == "" ]
+then
+    echo "${files[*]}" | dotnet trwdog -- php -d memory_limit=1G Test.php -q -x -tee -tree > parse.txt 2>&1
+else
+    echo "${files[*]}" | trwdog php -d memory_limit=1G Test.php -q -x -tee -tree > parse.txt 2>&1
+fi
 status=$?
 <endif>
 
