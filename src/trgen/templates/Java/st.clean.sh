8dc7c8e1005b890764d3be972b0a5dc54a05a82c
Ken Domino <kenneth.domino@domemtech.com>
2023-11-20 06:08:01 -0500

    Fix clean.sh in templates.

--- src/trgen/templates/Java/st.clean.sh
+++ src/trgen/templates/Java/st.clean.sh
@@ -3,9 +3,8 @@ version=4.13.1
 rm -f *.interp
 files=()
 JAR=`python -c "import os; from pathlib import Path; print(os.path.join(Path.home() , '.m2',  'repository', 'org', 'antlr', 'antlr4', '$version', 'antlr4-$version-complete.jar'))"`
-CLASSPATH="$JAR<if(path_sep_semi)>\;<else>:<endif>."
 <tool_grammar_tuples:{x |
-files+=( `java -jar -cp "$CLASSPATH" -depend -encoding <antlr_encoding> -Dlanguage=Java <x.AntlrArgs> <antlr_tool_args:{y | <y> } > <x.GrammarFileName> | awk '{print $1\}' | grep -v ':'` )
+files+=( `java -jar "$JAR" -depend -encoding <antlr_encoding> -Dlanguage=Java <x.AntlrArgs> <antlr_tool_args:{y | <y> } > <x.GrammarFileName> | awk '{print $1\}' | grep -v ':'` )
 } >
 for i in ${files[*]}
 do
