4a152bf86f091942929d896d1f56d5ecdf749f3c
Ken Domino <kenneth.domino@domemtech.com>
2024-07-09 19:56:10 -0400

    Clean up. Add additional filter evaluation.

--- src/tritext/Command.cs
+++ src/tritext/Command.cs
@@ -38,217 +38,224 @@ using System.Text;
 namespace Trash;
 
 class Command
+{
+    public string Help()
     {
-        public string Help()
+        using (Stream stream = this.GetType().Assembly.GetManifestResourceStream("tritext.readme.md"))
+        using (StreamReader reader = new StreamReader(stream))
         {
-            using (Stream stream = this.GetType().Assembly.GetManifestResourceStream("tritext.readme.md"))
-            using (StreamReader reader = new StreamReader(stream))
-            {
-                return reader.ReadToEnd();
-            }
-        }
-
-        static int max = 0;
-        static int key = 0;
-
-        static string GetTextFromPDF(string src_file_name, Config config)
-        {
-            // Perform two passes on the PDF file. The first pass is to determine
-            // the start of the left-hand side margin. The second pass extracts the
-            // text from the PDF file, using the left-hand side margin to make sure
-            // we have the appropriate number of spaces at the beginning of each line.
-
-            StringBuilder text = new StringBuilder();
-            {
-                {
-                    var pdfDoc = new PdfDocument(new PdfReader(src_file_name));
-                    var histogram = new BuildHistogram(config);
-                    for (int i = 1; i <= pdfDoc.GetNumberOfPages(); i++)
-                    {
-                        var pdfPage = pdfDoc.GetPage(i);
-                        try
-                        {
-                            PdfTextExtractor.GetTextFromPage(pdfPage, histogram);
-                        }
-                        catch (Exception)
-                        {
-                        }
-                    }
-                    foreach (var h in histogram.Histogram)
-                    {
-                        if (h.Value > max)
-                        {
-                            max = h.Value;
-                            key = h.Key;
-                        }
-                    }
-                }
-                {
-                    PdfDocument pdfDoc = new PdfDocument(new PdfReader(src_file_name));
-                    for (int i = 1; i <= pdfDoc.GetNumberOfPages(); i++)
-                    {
-                        var pdfPage = pdfDoc.GetPage(i);
-                        text.Append(PdfTextExtractor.GetTextFromPage(pdfPage, new MySimpleTextExtractionStrategy(config, key, max)));
-                    }
-                }
-            }
-            return text.ToString();
-        }
-
-
-        public void Execute(Config config)
-        {
-            if (config.Verbose)
-            {
-                System.Console.Error.WriteLine("reading from file >>>" + String.Join(", ", config.Files) + "<<<");
-            }
-            foreach (var f in config.Files)
-            {
-                System.Console.WriteLine(GetTextFromPDF(f, config));
-            }
+            return reader.ReadToEnd();
         }
     }
-    public class MyVisitor : FilterBaseVisitor<OneOf.OneOf<object, bool, int>>
+
+    static int max = 0;
+    static int key = 0;
+
+    static string GetTextFromPDF(string src_file_name, Config config)
     {
-        private int l1_;
-        private int l2_;
-        public MyVisitor(int l1, int l2)
+        // Perform two passes on the PDF file. The first pass is to determine
+        // the start of the left-hand side margin. The second pass extracts the
+        // text from the PDF file, using the left-hand side margin to make sure
+        // we have the appropriate number of spaces at the beginning of each line.
+
+        StringBuilder text = new StringBuilder();
         {
-            l1_ = l1;
-            l2_ = l2;
+            {
+                var pdfDoc = new PdfDocument(new PdfReader(src_file_name));
+                var histogram = new BuildHistogram(config);
+                for (int i = 1; i <= pdfDoc.GetNumberOfPages(); i++)
+                {
+                    var pdfPage = pdfDoc.GetPage(i);
+                    try
+                    {
+                        PdfTextExtractor.GetTextFromPage(pdfPage, histogram);
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+
+                foreach (var h in histogram.Histogram)
+                {
+                    if (h.Value > max)
+                    {
+                        max = h.Value;
+                        key = h.Key;
+                    }
+                }
+            }
+            {
+                PdfDocument pdfDoc = new PdfDocument(new PdfReader(src_file_name));
+                for (int i = 1; i <= pdfDoc.GetNumberOfPages(); i++)
+                {
+                    var pdfPage = pdfDoc.GetPage(i);
+                    text.Append(PdfTextExtractor.GetTextFromPage(pdfPage,
+                        new MySimpleTextExtractionStrategy(config, key, max)));
+                }
+            }
+        }
+        return text.ToString();
+    }
+
+
+    public void Execute(Config config)
+    {
+        if (config.Verbose)
+        {
+            System.Console.Error.WriteLine("reading from file >>>" + String.Join(", ", config.Files) + "<<<");
         }
 
-        public override OneOf<object, bool, int> VisitStart(FilterParser.StartContext context)
+        foreach (var f in config.Files)
         {
-            var result = new OneOf<object, bool, int>();
-            foreach (var e in context.expr())
+            System.Console.WriteLine(GetTextFromPDF(f, config));
+        }
+    }
+}
+
+public class MyVisitor : FilterBaseVisitor<OneOf.OneOf<object, bool, int>>
+{
+    private int l1_;
+    private int l2_;
+
+    public MyVisitor(int l1, int l2)
+    {
+        l1_ = l1;
+        l2_ = l2;
+    }
+
+    public override OneOf<object, bool, int> VisitStart(FilterParser.StartContext context)
+    {
+        var result = new OneOf<object, bool, int>();
+        foreach (var e in context.expr())
+        {
+            var v = Visit(e);
+            if (v.IsT1 && result.IsT0)
             {
-                var v = Visit(e);
-                if (v.IsT1 && result.IsT0)
-                {
-                    result = v;
-                }
-                else if (v.IsT1 && result.IsT1)
-                {
-                    result = result.AsT1 && v.AsT1;
-                }
-                else
-                {
-                    result = new OneOf<object, bool, int>();
-                }
+                result = v;
             }
-            return result;
+            else if (v.IsT1 && result.IsT1)
+            {
+                result = result.AsT1 || v.AsT1;
+            }
+            else
+            {
+                result = new OneOf<object, bool, int>();
+            }
         }
 
-        public override OneOf<object, bool, int> VisitExpr(FilterParser.ExprContext context)
+        return result;
+    }
+
+    public override OneOf<object, bool, int> VisitExpr(FilterParser.ExprContext context)
+    {
+        if (context.NUM() != null)
         {
-            if (context.NUM() != null)
+            return int.Parse(context.NUM().GetText());
+        }
+        else if (context.ID() != null && context.ID().GetText().Equals("l1"))
+        {
+            return l1_;
+        }
+        else if (context.ID() != null && context.ID().GetText().Equals("l2"))
+        {
+            return l2_;
+        }
+        else if (context.ID() != null)
+        {
+            return null;
+        }
+        else if (context.expr().Length == 2)
+        {
+            var v1 = VisitExpr(context.expr(0));
+            var v2 = VisitExpr(context.expr(1));
+            if (v1.IsT2 && v2.IsT2 && context.LT() != null)
             {
-                return int.Parse(context.NUM().GetText());
+                return v1.AsT2 < v2.AsT2;
             }
-            else if (context.ID() != null && context.ID().GetText().Equals("l1"))
+            else if (v1.IsT2 && v2.IsT2 && context.LE() != null)
             {
-                return l1_;
+                return v1.AsT2 <= v2.AsT2;
             }
-            else if (context.ID() != null && context.ID().GetText().Equals("l2"))
+            else if (v1.IsT2 && v2.IsT2 && context.GT() != null)
             {
-                return l2_;
+                return v1.AsT2 > v2.AsT2;
             }
-            else if (context.ID() != null)
+            else if (v1.IsT2 && v2.IsT2 && context.LE() != null)
             {
-                return null;
-            }
-            else if (context.expr().Length == 2)
-            {
-                var v1 = VisitExpr(context.expr(0));
-                var v2 = VisitExpr(context.expr(1));
-                if (v1.IsT2 && v2.IsT2 && context.LT() != null)
-                {
-                    return v1.AsT2 < v2.AsT2;
-                }
-                else if (v1.IsT2 && v2.IsT2 && context.LE() != null)
-                {
-                    return v1.AsT2 <= v2.AsT2;
-                }
-                else if (v1.IsT2 && v2.IsT2 && context.GT() != null)
-                {
-                    return v1.AsT2 > v2.AsT2;
-                }
-                else if (v1.IsT2 && v2.IsT2 && context.LE() != null)
-                {
-                    return v1.AsT2 >= v2.AsT2;
-                }
-                else
-                {
-                    return null;
-                }
+                return v1.AsT2 >= v2.AsT2;
             }
             else
             {
                 return null;
             }
         }
-    }
-
-
-    internal class BuildHistogram : ITextExtractionStrategy
-    {
-        private Config config;
-        public SortedDictionary<int, int> Histogram = new SortedDictionary<int, int>();
-        private FilterParser.StartContext tree;
-        
-        public BuildHistogram(Config c)
-        {
-            config = c;
-            // Parse config.Filter string to get l1 and l2 limitations.
-            ICharStream str = new AntlrInputStream(config.Filter);
-            FilterLexer lexer = new FilterLexer(str);
-            CommonTokenStream tokens = new CommonTokenStream(lexer);
-            FilterParser parser = new FilterParser(tokens);
-            ErrorListener<int> listener_lexer = new ErrorListener<int> (false, false, System.Console.Error);
-            ErrorListener<IToken> listener_parser = new ErrorListener<IToken> (false, false, System.Console.Error);
-            lexer.RemoveErrorListeners();
-            parser.RemoveErrorListeners();
-            lexer.AddErrorListener(listener_lexer);
-            parser.AddErrorListener(listener_parser);
-            tree = parser.start();
-        }
-
-        public virtual void EventOccurred(IEventData data, EventType type)
-        {
-            if (type.Equals(EventType.RENDER_TEXT))
-            {
-                TextRenderInfo renderInfo = (TextRenderInfo)data;
-                LineSegment segment = renderInfo.GetBaseline();
-                Vector start = segment.GetStartPoint();
-                Vector end = segment.GetEndPoint();
-                var l1 = (int)start.Get(0);
-                var l2 = (int)end.Get(1);
-                MyVisitor visitor = new MyVisitor(l1, l2);
-                var t = tree.Accept(visitor);
-                if (t.IsT1)
-                {
-                    if (t.AsT1)
-                    {
-                        return;
-                    }
-                }
-                var text = renderInfo.GetText();
-                if (config.OutputPreflight) System.Console.WriteLine("l1 " + l1 + " l2 " + l2 + " text " + text);
-                Histogram[l1] = Histogram.ContainsKey(l1) ? Histogram[l1] + 1 : 1;
-            }
-        }
-
-        public virtual ICollection<EventType> GetSupportedEvents()
+        else
         {
             return null;
         }
+    }
+}
 
-        public string GetResultantText()
+internal class BuildHistogram : ITextExtractionStrategy
+{
+    private Config config;
+    public SortedDictionary<int, int> Histogram = new SortedDictionary<int, int>();
+    private FilterParser.StartContext tree;
+
+    public BuildHistogram(Config c)
+    {
+        config = c;
+        // Parse config.Filter string to get l1 and l2 limitations.
+        ICharStream str = new AntlrInputStream(config.Filter);
+        FilterLexer lexer = new FilterLexer(str);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        FilterParser parser = new FilterParser(tokens);
+        ErrorListener<int> listener_lexer = new ErrorListener<int>(false, false, System.Console.Error);
+        ErrorListener<IToken> listener_parser = new ErrorListener<IToken>(false, false, System.Console.Error);
+        lexer.RemoveErrorListeners();
+        parser.RemoveErrorListeners();
+        lexer.AddErrorListener(listener_lexer);
+        parser.AddErrorListener(listener_parser);
+        tree = parser.start();
+    }
+
+    private static int xxxxxxx = 0;
+    public virtual void EventOccurred(IEventData data, EventType type)
+    {
+        if (type.Equals(EventType.RENDER_TEXT))
         {
-            return "";
+            TextRenderInfo renderInfo = (TextRenderInfo)data;
+            LineSegment segment = renderInfo.GetBaseline();
+            Vector start = segment.GetStartPoint();
+            Vector end = segment.GetEndPoint();
+            var l1 = (int)start.Get(0);
+            var l2 = (int)end.Get(1);
+            MyVisitor visitor = new MyVisitor(l1, l2);
+            var t = tree.Accept(visitor);
+            if (t.IsT1)
+            {
+                if (t.AsT1)
+                {
+                    return;
+                }
+            }
+
+            var text = renderInfo.GetText();
+            if (config.OutputPreflight) System.Console.WriteLine("l1 " + l1 + " l2 " + l2 + " text " + text);
+            Histogram[l1] = Histogram.ContainsKey(l1) ? Histogram[l1] + 1 : 1;
         }
     }
+
+    public virtual ICollection<EventType> GetSupportedEvents()
+    {
+        return null;
+    }
+
+    public string GetResultantText()
+    {
+        return "";
+    }
+}
 
 public class MySimpleTextExtractionStrategy : ITextExtractionStrategy
 {
@@ -280,6 +287,8 @@ public class MySimpleTextExtractionStrategy : ITextExtractionStrategy
 
     private readonly StringBuilder result = new StringBuilder();
     private static string emphasis = "";
+    private static int xxxxxxx = 0;
+    private static int uuuuuuu = 0;
 
     public virtual void EventOccurred(IEventData data, EventType type)
     {
@@ -291,6 +300,26 @@ public class MySimpleTextExtractionStrategy : ITextExtractionStrategy
             Vector end = segment.GetEndPoint();
             var l1 = (int)start.Get(0);
             var l2 = (int)end.Get(1);
+            if (lastStart == null)
+            {
+                lastStart = start;
+            }
+            if (lastEnd == null)
+            {
+                lastEnd = end;
+            }
+            var x = renderInfo.GetText();
+            if (x.Contains("J3/23-007r1"))
+            {
+                xxxxxxx++;
+                uuuuuuu++;
+            }
+            if (uuuuuuu >= 104)
+            {
+            //    System.Console.WriteLine(uuuuuuu + " " + xxxxxxx + " " + x + " " + l1 + " " + l2); 
+                xxxxxxx++;
+            }
+
             MyVisitor visitor = new MyVisitor(l1, l2);
             var t = tree.Accept(visitor);
             if (t.IsT1)
@@ -301,7 +330,6 @@ public class MySimpleTextExtractionStrategy : ITextExtractionStrategy
                 }
             }
 
-            var x = renderInfo.GetText();
             var f = renderInfo.GetFont();
             var fp = f.GetFontProgram();
             var fn = fp.GetFontNames();
@@ -367,6 +395,10 @@ public class MySimpleTextExtractionStrategy : ITextExtractionStrategy
             if (!firstRender)
             {
                 Vector x1 = lastStart;
+                if (x1 == null)
+                {
+                    x1 = start;
+                }
                 Vector x2 = lastEnd;
                 // see http://mathworld.wolfram.com/Point-LineDistance2-Dimensional.html
                 float dist = (x2.Subtract(x1)).Cross((x1.Subtract(start))).LengthSquared() / x2.Subtract(x1)
