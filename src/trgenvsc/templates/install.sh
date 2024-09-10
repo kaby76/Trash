4e537dc910c4cbec4be3990fe67d77d52be314ef
Ken Domino <kenneth.domino@domemtech.com>
2024-09-10 06:52:30 -0400

    Bring packages for vsc extension up to date.

--- src/trgenvsc/templates/install.sh
+++ src/trgenvsc/templates/install.sh
@@ -14,11 +14,17 @@ popd
 if [[ "$OSTYPE" == "darwin"* ]]; then
         brew install vsce
 fi
-npm i vscode-jsonrpc@6.0.0-next.5
-npm i vscode-languageclient@7.0.0-next.9
-npm i vscode-languageserver@7.0.0-next.7
-npm i vscode-languageserver-protocol@3.16.0-next.7
-npm i vscode-languageserver-types@3.16.0-next.3
+npm i vscode-jsonrpc
+npm i vscode-languageclient
+npm i vscode-languageserver
+npm i vscode-languageserver-protocol
+npm i vscode-languageserver-types
+npm i jsonc-parser
+npm i @types/vscode
+npm i @types/mocha
+npm i @types/node
+npm i @vscode/vsce
+
 npm install
 npm run compile
 ./node_modules/.bin/vsce package
