bbc20335e82989bb12196721864766931d230f72
Ken Domino <kenneth.domino@domemtech.com>
2024-09-18 10:25:19 -0400

    Fix test.

--- _tests/grammars-v4/test.sh
+++ _tests/grammars-v4/test.sh
@@ -9,7 +9,12 @@ do
 	for target in Antlr4ng Cpp CSharp Dart Go Java JavaScript Python3 TypeScript
 	do
 		dotnet trgen -- -t $target
-		pushd Generated-$target*
+		# If a target is not available, exit code should zero, but Generated directory won't exist.
+		if [ ! -d Generated-$target ]
+		then
+			continue
+		fi
+		pushd Generated-$target
 		make
 		make test
 		pwsh test.ps1
