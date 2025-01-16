016889301f75c86580e7e467c6861976d40f92bc
Ken Domino <kenneth.domino@domemtech.com>
2025-01-16 07:02:19 -0500

    Add debugging info.

--- src/trgen/templates/Antlr4ng/st.build.sh
+++ src/trgen/templates/Antlr4ng/st.build.sh
@@ -1,5 +1,6 @@
 # Generated from trgen <version>
 set -e
+set -x
 rm -rf node_modules package-lock.json
 npm install -g typescript ts-node
 npm install
@@ -11,6 +12,9 @@ if [ -f transformGrammar.py ]; then python3 transformGrammar.py ; fi
 # version number. We can then use this with antlr4 to generate the
 # parser and lexer.
 version=`grep antlr4 package.json | awk '{print $2}' | tr -d '"' | tr -d ',' | tr -d '\r' | tr -d '\n'`
+
+ls node_modules
+ls node_modules/.bin
 
 <tool_grammar_tuples:{x |
 pwsh node_modules/.bin/antlr-ng.ps1 --encoding <antlr_encoding> -Dlanguage=TypeScript <x.AntlrArgs> <antlr_tool_args:{y | <y> } > <x.GrammarFileName>
