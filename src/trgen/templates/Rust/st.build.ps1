1651210841bffac201d4e2a90a27fae0588d36ab
Ken Domino <kenneth.domino@domemtech.com>
2025-10-30 19:09:39 -0400

    Update Rust templates to be in sync with grammars-v4.

--- src/trgen/templates/Rust/st.build.ps1
+++ src/trgen/templates/Rust/st.build.ps1
@@ -25,6 +25,10 @@ if($compile_exit_code -ne 0){
 \}
 }>
 
+<if(os_win)>
 $env:RUSTFLAGS = "-C link-arg=/STACK:16777216"
+<else>
+<endif>
+
 $(& cargo b --release ; $compile_exit_code = $LASTEXITCODE) | Write-Host
 exit $compile_exit_code
