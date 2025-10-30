77bf12d7bba9becc74a563fbc9e7e78a43e319fb
Ken Domino <kenneth.domino@domemtech.com>
2025-10-30 08:44:59 -0400

    Updates for Rust target.

--- src/trgen/templates/Rust/run.sh
+++
@@ -1,1 +0,0 @@
-./target/release/test.exe "$@"
--- src/trgen/templates/Rust/st.build.ps1
+++ src/trgen/templates/Rust/st.build.ps1
@@ -1,47 +1,30 @@
 # Generated from trgen <version>
 
-$version="4.13.1"
-$env:GO111MODULE = "on"
-For ($i=0; $i -le 5; $i++) {
-	$(& go get github.com/antlr4-go/antlr/v4@v$version ; $compile_exit_code = $LASTEXITCODE) | Write-Host
-	if($compile_exit_code -eq 0){
-		Break
-	}
-	Write-Host "go get failed. Trying again."
-}
-if($compile_exit_code -ne 0){
-    exit $compile_exit_code
-}
-For ($i=0; $i -le 5; $i++) {
-	$(& go get golang.org/x/text@v0.26.0 ; $compile_exit_code = $LASTEXITCODE) | Write-Host
-	if($compile_exit_code -eq 0){
-		Break
-	}
-	Write-Host "go get failed. Trying again."
-}
-if($compile_exit_code -ne 0){
-    exit $compile_exit_code
-}
-
 if (Test-Path -Path transformGrammar.py -PathType Leaf) {
     $(& python3 transformGrammar.py ) 2>&1 | Write-Host
 }
 
-<if(antlrng_tool)>
-npm init -y
-npm i antlr-ng
-<endif>
+$JAR = "antlr4-4.13.3-SNAPSHOT-complete.jar"
+if (-not (Test-Path -Path $JAR -PathType Leaf)) {
+    For ($i=0; $i -le 5; $i++) {
+	$(& curl -L -O https://github.com/antlr4rust/antlr4/releases/download/v0.5.0/$JAR ; $compile_exit_code = $LASTEXITCODE) | Write-Host
+	if($compile_exit_code -eq 0){
+		Break
+	}
+	Write-Host "go get failed. Trying again."
+    }
+}
+if (-not (Test-Path -Path $JAR -PathType Leaf)) {
+    exit 1
+}
 
 <tool_grammar_tuples:{x |
-<if(antlrng_tool)>
-$(& tsx $HOME/antlr-ng/cli/runner.ts --encoding <antlr_encoding> -Dlanguage=Go --lib parser --package parser <x.AntlrArgs> <antlr_tool_args:{y | <y> } > <x.GrammarFileNameTarget> ; $compile_exit_code = $LASTEXITCODE) | Write-Host
-<else>
-$(& antlr4 -v $version <x.GrammarFileNameTarget> -encoding <antlr_encoding> -Dlanguage=Go <if(os_win)>-o parser<endif> -lib parser -package parser  <x.AntlrArgs>  <antlr_tool_args:{y | <y> } > ; $compile_exit_code = $LASTEXITCODE) | Write-Host
-<endif>
+$(& java -jar $JAR -encoding <antlr_encoding> -Dlanguage=Rust -o src/gen <x.AntlrArgs>  <antlr_tool_args:{y | <y> } >  <x.GrammarFileNameTarget> ; $compile_exit_code = $LASTEXITCODE) | Write-Host
 if($compile_exit_code -ne 0){
     exit $compile_exit_code
 \}
 }>
 
-$(& go build ; $compile_exit_code = $LASTEXITCODE) | Write-Host
+$env:RUSTFLAGS = "-C link-arg=/STACK:16777216"
+$(& cargo b --release ; $compile_exit_code = $LASTEXITCODE) | Write-Host
 exit $compile_exit_code
