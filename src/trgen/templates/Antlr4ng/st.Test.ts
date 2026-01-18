01e48c234e9608468c6844641d7c7599c55278ca
Ken Domino <kenneth.domino@domemtech.com>
2026-01-18 07:27:37 -0500

    Fix template for Antlr4ng.

--- src/trgen/templates/Antlr4ng/st.Test.ts
+++ src/trgen/templates/Antlr4ng/st.Test.ts
@@ -106,9 +106,11 @@ function main() {
                 show_trace = true;
                 break;
             default:
-                inputs.push(process.argv[i]);
-                is_fns.push(true);
                 break;
+        }
+        if (process.argv[i][0] !== '-') {
+            inputs.push(process.argv[i]);
+            is_fns.push(true);
         }
     }
     if (inputs.length == 0) {
