51651d78d6066462c452621a1a2fbd8ff203e5c5
kaby76 <kenneth.domino@domemtech.com>
2026-05-29 14:59:44 -0400

    Fix install of trash tool.

--- _scripts/install-local.sh
+++ _scripts/install-local.sh
@@ -2,13 +2,5 @@
 set -x
 dotnet new tool-manifest --force
 cd src
-exes=`find . -name 'tr*.exe' | grep -v publish`
-for i in $exes
-do
-	d=`echo $i | awk -F '/' '{print $2}'`
-	pushd $d
-	tool=${d##*/}
-	dotnet tool install $tool
-	dotnet $tool --version
-	popd
-done
+dotnet tool install trash
+dotnet trash --version
