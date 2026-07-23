c2a8464a3a8fe2b1dd08d986a5951b53760cd9a2
kaby76 <kenneth.domino@domemtech.com>
2026-07-23 19:32:04 -0400

    Delete temp directory 'n' before generating to.

--- src/trinterp/examples/check.sh
+++ src/trinterp/examples/check.sh
@@ -4,7 +4,7 @@
 # Get full path of this script.
 full_path_script=$(realpath $0)
 full_path_script_dir=`dirname $full_path_script`
-rm -rf a i
+rm -rf a n i
 mkdir a n i
 antlr4 -v 4.13.2 -atn -Dlanguage=CSharp -o a *.g4
 antlr-ng --atn true -o n -Dlanguage=None *.g4
