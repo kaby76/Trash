2db897349361a73a3a988ba8b5f42cfaf080dbe9
Ken Domino <kenneth.domino@domemtech.com>
2026-07-23 07:39:48 -0400

    Clean ups.

--- src/trinterp/examples/check.sh
+++ src/trinterp/examples/check.sh
@@ -8,21 +8,3 @@ rm -rf a i
 mkdir a i
 antlr4 -v 4.13.2 -atn -Dlanguage=CSharp -o a *.g4
 dotnet trash parse *.g4 | dotnet $full_path_script_dir/../bin/Release/net10.0/trinterp.dll --atn -o i
-
-pushd a
-for d in `ls *.dot`
-do
-	python /c/Users/Kenne/Documents/GitHub/Trash/sort-dot.py $d > xxx
-	mv xxx $d
-done
-popd
-
-pushd i
-for d in `ls *.dot`
-do
-	python /c/Users/Kenne/Documents/GitHub/Trash/sort-dot.py $d > xxx
-	mv xxx $d
-done
-popd
-
-diff -r a i
