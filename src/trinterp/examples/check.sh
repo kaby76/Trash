da2b533aeb115457f4c8dbad2e4722e7f46729a7
kaby76 <kenneth.domino@domemtech.com>
2026-07-23 15:59:20 -0400

    Fix dependencies.

--- src/trinterp/examples/check.sh
+++ src/trinterp/examples/check.sh
@@ -5,6 +5,7 @@
 full_path_script=$(realpath $0)
 full_path_script_dir=`dirname $full_path_script`
 rm -rf a i
-mkdir a i
+mkdir a n i
 antlr4 -v 4.13.2 -atn -Dlanguage=CSharp -o a *.g4
+antlr-ng --atn true -o n -Dlanguage=None *.g4
 dotnet trash parse *.g4 | dotnet $full_path_script_dir/../bin/Release/net10.0/trinterp.dll --atn -o i
