7f050b30ead0a2056d3c0099af530dee8a92d1ce
kaby76 <kenneth.domino@domemtech.com>
2026-07-24 06:54:54 -0400

    Added new tests for trinterp. Updated test-interp.sh to include Antlr-ng .dot compares.

--- src/trinterp/test-interp.sh
+++ src/trinterp/test-interp.sh
@@ -13,8 +13,20 @@ done < <(find . -name desc.xml -not -path '*/.git/*' | grep -v Generated | sort 
 
 for grammar in "${grammars[@]}"; do
     pushd $grammar
+
+    rm -rf a i n
+
+    # Generate ATN .dot files
     antlr4 -v 4.13.2 -atn -Dlanguage=CSharp -o a *.g4
+    antlr-ng --atn true -Dlanguage=None -o n *.g4
     dotnet trash parse *.g4 | dotnet trash interp --atn -o i
+
+    echo diff between Antlr4 and Antlr-ng
+    python $full_path_script_dir/compare-atn.py a n
+
+    echo diff between Antlr4 and Trinterp
     python $full_path_script_dir/compare-atn.py a i
+
+    rm -rf a i n
     popd
 done
