b8287d6fd049c5f564655d252fb8d1cbe7d7de9a
Ken Domino <kenneth.domino@domemtech.com>
2026-08-26 20:02:10 -0400

    fix(trparse): preserve escaped lexer delimiters

--- tests/AllStarParserTests/LexerCommandTests.cs
+++ tests/AllStarParserTests/LexerCommandTests.cs
@@ -108,6 +108,24 @@ public class LexerCommandTests
         Assert.Equal(["value", "=", "TRUE", "\n"], tokens);
     }
 
+    [Fact]
+    public void NonGreedyLexerLoopPreservesHigherPriorityEscapedDelimiter()
+    {
+        var lexerInterp = Path.Combine(
+            AppContext.BaseDirectory, "TestData", "corundum", "interp",
+            "CorundumLexer.interp");
+        var interp = InterpFileReader.Read(File.ReadAllText(lexerInterp));
+        var atn = AtnDeserializer.Deserialize(interp.AtnData);
+
+        var literals = new LexerAtnSimulator(atn)
+            .Tokenize("\"first\" \"Hello, I'm CORUN\\\"DUM\"")
+            .Where(token => token.Channel == 0 && token.Type != -1)
+            .Select(token => token.Text)
+            .ToArray();
+
+        Assert.Equal(["\"first\"", "\"Hello, I'm CORUN\\\"DUM\""], literals);
+    }
+
     private static MyATN BuildCommandAtn() => BuildAtn(
         modeCount: 3,
         new RuleSpec(0, '<', 1, new(MyLexerActionType.PushMode, 1, 0)),
